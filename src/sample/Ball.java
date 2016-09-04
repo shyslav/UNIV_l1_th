840c3f90b351590fcf73c8ff885d2c4e8f8ff335
shyslav <shyslav@gmail.com>
2016-09-04 12:37:41 +0300

    added the ball array

--- src/sample/Ball.java
+++ src/sample/Ball.java
@@ -6,10 +6,43 @@ import javafx.scene.paint.*;
  * Created by shyslav on 9/3/16.
  */
 public class Ball extends Circle{
-    private final int ballRadius = 20;
-
-    public Ball(double radius, Color fill) {
-        super(radius, fill);
+    private static double ballRadius = 15;
+    private final int lifeTime = 60;
+    private int deltaX = 3;
+    private int deltaY = 3;
+    private long startTime;
+    private int sleepTime;
+    public Ball(Color fill) {
+        super(ballRadius, fill);
+        this.startTime = System.currentTimeMillis();
+        this.sleepTime = 15;
     }
 
+    public long increaseSleepTime() {
+        if(System.currentTimeMillis() - startTime >10000){
+            startTime = System.currentTimeMillis();
+            sleepTime++;
+        }
+        return sleepTime;
+    }
+
+    public int getSleepTime() {
+        return sleepTime;
+    }
+
+    public int getDeltaX() {
+        return deltaX;
+    }
+
+    public void setDeltaX(int deltaX) {
+        this.deltaX = deltaX;
+    }
+
+    public int getDeltaY() {
+        return deltaY;
+    }
+
+    public void setDeltaY(int deltaY) {
+        this.deltaY = deltaY;
+    }
 }
--- src/sample/Main.java
+++ src/sample/Main.java
@@ -1,5 +1,6 @@
 package sample;
 
+import javafx.animation.AnimationTimer;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.geometry.Bounds;
@@ -13,11 +14,10 @@ import java.util.ArrayList;
 
 
 public class Main extends Application {
+    private AnimationTimer timer;
     private static ArrayList<Ball> ballArray;
     private long startTime;
     private static Pane root;
-    private static double deltaX = 3;
-    private static double deltaY = 3;
 
     @Override
     public void start(Stage primaryStage) throws Exception {
@@ -33,55 +33,66 @@ public class Main extends Application {
 
         addBallToPane();
 
-        Thread tr = new Thread(() -> {
-            while (true) {
-                try {
-                    Thread.sleep(15);
-                } catch (InterruptedException e) {
-
+        timer = new AnimationTimer() {
+            @Override
+            public void handle(long now) {
+                for (Ball bal : ballArray) {
+                    try {
+                        Thread.sleep(bal.getSleepTime());
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                    move(bal);
                 }
-                move();
             }
-        });
-        tr.start();
+        };
+        timer.start();
         scene.setOnKeyPressed(event -> {
-            if(event.getCode() == KeyCode.SPACE){
+            if (event.getCode() == KeyCode.SPACE) {
                 addBallToPane();
             }
         });
     }
 
     private static void addBallToPane() {
-        synchronized (ballArray) {
-            Ball ball = new Ball(15, Color.BLUE);
-            ball.relocate(100, 100);
-            ballArray.add(ball);
-            root.getChildren().addAll(ball);
+        Ball ball = new Ball(Color.GREEN);
+        ball.relocate(100, 100);
+        ballArray.add(ball);
+        root.getChildren().addAll(ball);
+    }
+
+    private static void move(Ball bal) {
+        for (int i = 0; i < ballArray.size(); i++) {
+            if (i + 1 < ballArray.size() && ballArray.get(i).getBoundsInParent().intersects(ballArray.get(i + 1).getBoundsInParent())) {
+                rotate(ballArray.get(i), ballArray.get(i + 1));
+            }
+        }
+        //Can use TimeLine, but cant change duration in this class
+        bal.increaseSleepTime();
+
+        bal.setLayoutX(bal.getLayoutX() + bal.getDeltaX());
+        bal.setLayoutY(bal.getLayoutY() + bal.getDeltaY());
+
+        final Bounds bounds = root.getBoundsInLocal();
+        final boolean atRightBorder = bal.getLayoutX() >= (bounds.getMaxX() - bal.getRadius());
+        final boolean atLeftBorder = bal.getLayoutX() <= (bounds.getMinX() + bal.getRadius());
+        final boolean atBottomBorder = bal.getLayoutY() >= (bounds.getMaxY() - bal.getRadius());
+        final boolean atTopBorder = bal.getLayoutY() <= (bounds.getMinY() + bal.getRadius());
+
+        if (atRightBorder || atLeftBorder) {
+            bal.setDeltaX(bal.getDeltaX() * -1);
+        }
+        if (atBottomBorder || atTopBorder) {
+            bal.setDeltaY(bal.getDeltaY() * -1);
         }
     }
 
-    private static void move() {
-        //Can use TimeLine, but cant change duration in this class
-        synchronized (ballArray) {
-            for (int i = 0 ; i < ballArray.size(); i++){
-                Ball bal = ballArray.get(i);
-                bal.setLayoutX(bal.getLayoutX() + deltaX);
-                bal.setLayoutY(bal.getLayoutY() + deltaY);
+    public static void rotate(Ball b1, Ball b2) {
+        b1.setDeltaX(b1.getDeltaX() * -1);
+        b1.setDeltaY(b1.getDeltaY() * -1);
 
-                final Bounds bounds = root.getBoundsInLocal();
-                final boolean atRightBorder = bal.getLayoutX() >= (bounds.getMaxX() - bal.getRadius());
-                final boolean atLeftBorder = bal.getLayoutX() <= (bounds.getMinX() + bal.getRadius());
-                final boolean atBottomBorder = bal.getLayoutY() >= (bounds.getMaxY() - bal.getRadius());
-                final boolean atTopBorder = bal.getLayoutY() <= (bounds.getMinY() + bal.getRadius());
-
-                if (atRightBorder || atLeftBorder) {
-                    deltaX *= -1;
-                }
-                if (atBottomBorder || atTopBorder) {
-                    deltaY *= -1;
-                }
-            }
-        }
+        b2.setDeltaX(b1.getDeltaX() * -1);
+        b2.setDeltaY(b1.getDeltaY() * -1);
     }
 
     public long getDuration() {
